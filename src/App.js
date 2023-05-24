f119c9640a06dd940b240cef1559fe5e3341f2c1
Karan P <karan.pawar@coditas.com>
2023-05-24 17:22:06 +0530

    skeleton

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import './App.css';
+import Body from './components/Body';
+import Header from './components/Header';
 
 function App() {
   return (
     <div>
       <h1 className='text-3xl'>Youtube App</h1>
+      <Header />
+      <Body />
       {/* 
       head
       body
@@ -13,7 +17,7 @@ function App() {
           buttons list 
           video-container
             video card
-            
+
        */}
     </div>
   );
